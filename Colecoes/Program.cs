e9eafd1ca5d52639a8868fd7d5523d2fb7b6715c
origin <marlon.dias@ufpe.br>
2022-01-12 12:21:49 -0300

    Adicionar arquivos de projeto.

---
+++ Colecoes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayInteiros = new int[3];
+
+            arrayInteiros [0] = 10;
+            arrayInteiros [1] = 20;
+            arrayInteiros [2] = 30;
+
+            Console.WriteLine("Parcorrendo o array pelo For");
+            for (int i = 0; i < arrayInteiros.Length ; i++)// Usar sempre a propriedade "Length" para tornar o "For" dinâmico;
+            {
+                Console.WriteLine(arrayInteiros[i]);
+            }
+
+            Console.WriteLine("Parcorrendo o array pelo ForEach");// Modo de percorre pelo "ForEach";
+            foreach (int item in arrayInteiros)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
