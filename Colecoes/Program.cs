790f6709976895b95ea3439a8b0f18d1d3655978
origin <marlon.dias@ufpe.br>
2022-01-12 13:22:33 -0300

    Update exercises

--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -1,28 +1,50 @@
-﻿using System;
-
-namespace Colecoes
+﻿namespace Colecoes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] arrayInteiros = new int[3];
-
-            arrayInteiros [0] = 10;
-            arrayInteiros [1] = 20;
-            arrayInteiros [2] = 30;
-
-            Console.WriteLine("Parcorrendo o array pelo For");
-            for (int i = 0; i < arrayInteiros.Length ; i++)// Usar sempre a propriedade "Length" para tornar o "For" dinâmico;
+            // Criando uma array multidimencional (Matriz);
+            int[,] matriz = new int[4, 2]
             {
-                Console.WriteLine(arrayInteiros[i]);
+                {8,8},
+                {10,20},
+                {50,100},
+                {90,200},
+            };
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.WriteLine(matriz[i, j]);
+                }
+
             }
 
-            Console.WriteLine("Parcorrendo o array pelo ForEach");// Modo de percorre pelo "ForEach";
-            foreach (int item in arrayInteiros)
-            {
-                Console.WriteLine(item);
+
+            //int[] arrayInteiros = new int[3];
+
+            //arrayInteiros [0] = 10;
+            //arrayInteiros [1] = 20;
+            //arrayInteiros [2] = int.Parse("30");
+
+            
+            //arrayInteiros [3] = 30; // Este elemento não existe no array
+
+            //// Modo de percorrer o array pelo "For";
+            //Console.WriteLine("Parcorrendo o array pelo For");
+            //for (int i = 0; i < arrayInteiros.Length ; i++)// Usar sempre a propriedade "Length" para tornar o "For" dinâmico;
+            //{
+            //    Console.WriteLine(arrayInteiros[i]);
+            //}
+
+            //// Modo de percorrer o array pelo "ForEach";
+            //Console.WriteLine("Parcorrendo o array pelo ForEach");
+            //foreach (int item in arrayInteiros)
+            //{
+            //    Console.WriteLine(item);
             }
         }
     }
-}
+
