574bd74da6e7f79fc05db507e1c87cf218338214
origin <marlon.dias@ufpe.br>
2022-01-12 23:51:45 -0300

    Update methods and exercises

---
+++ Colecoes/Helper/OperacoesArray.cs
@@ -0,0 +1,63 @@
+﻿namespace Colecoes.Helper
+{
+
+	public class OperacoesArray
+	{
+        // Implementação do organizador lógico de arrays "BubbleSort";
+        public void OrdenarBubbleSort(ref int[] array)
+        {
+            int temp = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length -1; j++)
+                {
+                    if(array[j] > array [j + 1])
+                    {
+                        temp = array[j + 1]; // Variável temporária para tornar possível as trocas de parâmetros dentro do array ao usar o BoobleSort;
+                        array[j + 1] = array [j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
+		public void ImprimirArray(int[] array)
+        {
+            var linha = string.Join(", ", array); // Transformando o array em uma liha separado por vírgula;
+            Console.WriteLine(linha);
+        }
+
+        public void Ordenar(ref int[] array)
+        {
+            Array.Sort(array);
+        }
+
+        // Método para copiar o array utilizando o Arraay.Copy
+        public void Copiar(ref int[] array, ref int[] arrayDestino)
+        {
+            Array.Copy(array, arrayDestino, array.Length);
+        }
+
+        // Método Array.Exists
+        public bool Existe(int [] array, int valor)
+        {
+            return Array.Exists(array, elemento => elemento == valor);
+        }
+
+        // Método para verificar toda a lista de array utilizando o "Array.TrueForAll";
+        public bool TodosMaiorQue(int[] array, int valor)
+        {
+            return Array.TrueForAll(array, elemento => elemento > valor);
+        }
+
+        // Criando método para encontrar um valor dentro do array utilizando o Array.Find;
+        public int ObterValor(int[] array, int valor)
+        {
+            return Array.Find(array, element => element == valor);
+        }
+	}
+
+}
+
+
--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -1,26 +1,105 @@
-﻿namespace Colecoes
+﻿using Colecoes.Helper;
+
+namespace Colecoes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Criando uma array multidimencional (Matriz);
-            int[,] matriz = new int[4, 2]
-            {
-                {8,8},
-                {10,20},
-                {50,100},
-                {90,200},
-            };
 
-            for (int i = 0; i < matriz.GetLength(0); i++)
-            {
-                for (int j = 0; j < matriz.GetLength(1); j++)
-                {
-                    Console.WriteLine(matriz[i, j]);
-                }
+            // Programa de ordenação de array;
+            OperacoesArray op = new OperacoesArray();
 
-            }
+            int[] array = new int[5] { 6, 3, 8, 1, 9 };
+            int[] arrayCopia = new int[10];
+
+
+
+            //// Programa para encontrar o valor específico no array;
+            //int valorProcurado = 9; // Alterar este valor para testar;
+
+            //int valorAchado = op.ObterValor(array, valorProcurado);
+
+            //if (valorAchado > 0)
+            //{
+            //    Console.WriteLine("Encontrei o valor");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Não encontrei o valor");
+            //}
+
+
+
+            ////Programa para verificar todo o array;
+            //int ValorProcurado = 0; // Alterar o valor de "0" para testar;
+
+            //bool todosMaiorQue = op.TodosMaiorQue(array, ValorProcurado);
+
+            //if (todosMaiorQue)
+            //{
+            //    Console.WriteLine("Todos os valores são maior que {0}", ValorProcurado);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Existem valores que não são maiorres do que {0}", ValorProcurado);
+            //}
+
+
+            //// Programa para verificar a existencia de algum elemento;
+            //int ValorProcurado = 10;
+            //bool existe = op.Existe(array, ValorProcurado);
+
+            //if (existe)
+            //{
+            //    Console.WriteLine("Encontrei o valor: {0}", ValorProcurado);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Não Encontrei o valor: {0}", ValorProcurado);
+            //}       
+                          
+                           
+
+            //// Copiando arrays (array para arrayCopia);
+            //Console.WriteLine("Array antes da cópia:");
+            //op.ImprimirArray(arrayCopia);
+
+            //op.Copiar(ref array, ref arrayCopia);
+            //Console.WriteLine("Array  após a cópia:");
+            //op.ImprimirArray(arrayCopia);
+
+            //Console.WriteLine("Array original:");
+            //op.ImprimirArray(array);
+
+            // Programa de ordenação de array usando o "BubbleSort";
+            //op.OrdenarBubbleSort(ref array);
+
+            // Ordenação através da Classe Array utilizando o método "Sort";
+            // op.Ordenar(ref array); 
+
+            //Console.WriteLine("Array ordenado:");
+            //op.ImprimirArray(array);
+
+
+            //// Criando uma array multidimencional (Matriz);
+            //int[,] matriz = new int[4, 2]
+            //{
+            //    {8,8},
+            //    {10,20},
+            //    {50,100},
+            //    {90,200},
+            //};
+
+            // Percorrendo uma Matriz com DOIS "For";
+            //for (int i = 0; i < matriz.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < matriz.GetLength(1); j++)
+            //    {
+            //        Console.WriteLine(matriz[i, j]);
+            //    }
+
+            //}
 
 
             //int[] arrayInteiros = new int[3];
@@ -29,7 +108,7 @@
             //arrayInteiros [1] = 20;
             //arrayInteiros [2] = int.Parse("30");
 
-            
+
             //arrayInteiros [3] = 30; // Este elemento não existe no array
 
             //// Modo de percorrer o array pelo "For";
@@ -44,7 +123,7 @@
             //foreach (int item in arrayInteiros)
             //{
             //    Console.WriteLine(item);
-            }
+        }
         }
     }
 
